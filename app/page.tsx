6e75591d80ae1d02bb8125b5c67aeee165c42eff
Will S1200 <wil19558@gmail.com>
2023-04-17 17:47:13 -0400

    Cleanup and typos

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import Image from 'next/image';
 // import { getBlogViews, getTweetCount, getStarCount } from 'lib/metrics';
 import {
   ArrowIcon,
-  GitHubIcon,
-  TwitterIcon,
-  ViewsIcon,
 } from 'components/icons';
 import PageTitle from 'components/page-title';
 import avatar from '../public/profile-square.jpg';
@@ -13,17 +10,6 @@ import avatar from '../public/profile-square.jpg';
 export const revalidate = 60;
 
 export default async function HomePage() {
-  let starCount, views, tweetCount;
-
-  // try {
-  //   [starCount, views, tweetCount] = await Promise.all([
-  //     getStarCount(),
-  //     getBlogViews(),
-  //     getTweetCount(),
-  //   ]);
-  // } catch (error) {
-  //   console.error(error);
-  // }
 
   return (
     <section className="max-w-[600px]">
@@ -48,28 +34,6 @@ export default async function HomePage() {
           <p>5+ years of backend <span className="dark:text-sky">Python</span> experience</p>
           <p>Business intelligence and analytics engineering</p>
           <p>Algorithmic crypto trading using <a className="dark:text-sky underline" href="/projects">Hummingbot</a></p>
-          {/* <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://twitter.com/leeerob"
-            className="flex items-center gap-2"
-          >
-            <TwitterIcon />
-            {`${tweetCount.toLocaleString()} tweets all time`}
-          </a>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/leerob"
-            className="flex items-center gap-2"
-          >
-            <GitHubIcon />
-            {`${starCount.toLocaleString()} stars on this repo`}
-          </a>
-          <Link href="/blog" className="flex items-center">
-            <ViewsIcon />
-            {`${views.toLocaleString()} blog views all time`}
-          </Link> */}
         </div>
       </div>
       <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-sky hover:opacity-70">
@@ -95,10 +59,10 @@ export default async function HomePage() {
         Dataroc is a portmanteau of <span className="text-sky">data</span> and part of my last name, La<span className="text-sky">roc</span>he :-)
       </p>
       <h2 className="my-5" id="why-dataroc">
-        What is that avatar?
+        What is that avatar/favicon?
       </h2>
       <p>
-        The avatar is named SOLEnOID and is the result of the creative genius of <a className="dark:text-sky underline" href="https://twitter.com/Logic_Beach">@logicbeach.eth</a>. It's the only NFT I possess, and proudly so!
+        The avatar is named SOLeNOID and is the result of the creative genius of <a className="dark:text-sky underline" href="https://twitter.com/Logic_Beach">@logicbeach.eth</a>. It's the only NFT I possess, and proudly so!
         You can find <a className="dark:text-sky underline" href="https://opensea.io/collection/logicbots-u1">the whole collection here</a>. It's also the inspiration of this website's color palette!
       </p>
       <hr className="my-10" />
