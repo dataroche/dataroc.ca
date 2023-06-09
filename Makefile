a54cee7ceb6868899364eb76c639cdb200585a84
Will S1200 <wil19558@gmail.com>
2023-06-08 22:27:55 -0400

    Postgrest and portfolio_history table

---
+++ Makefile
@@ -0,0 +1,15 @@
+SHELL := /bin/bash
+
+graphile-migrate-watch:
+	source .env.local && graphile-migrate watch
+
+frontend:
+	source .env.local && npm start
+
+postgrest:
+	source .env.local && docker run --rm --net=host \
+		-e PGRST_DB_URI=${PGRST_DB_URI} \
+		-e PGRST_SERVER_PORT=5001 \
+		-e PGRST_DB_SCHEMA=api \
+		-e PGRST_DB_ANON_ROLE=api_anon \
+		postgrest/postgrest
--- migrations/current.sql
+++ migrations/current.sql
@@ -1,8 +1,22 @@
+DROP VIEW IF EXISTS api.portfolio_history;
+
 DROP TABLE IF EXISTS portfolio_history;
 
 DROP TYPE IF EXISTS market_enum;
 
 DROP TYPE IF EXISTS interval_enum;
+
+DROP SCHEMA IF EXISTS api CASCADE;
+
+DROP ROLE IF EXISTS api_anon;
+
+CREATE SCHEMA api;
+
+CREATE ROLE api_anon nologin;
+
+GRANT api_anon TO authenticator;
+
+GRANT USAGE on SCHEMA api to api_anon;
 
 CREATE TYPE market_enum as enum('kraken');
 
@@ -16,5 +30,20 @@ CREATE TABLE portfolio_history (
     usd_total_value NUMERIC(48, 18),
     usd_held NUMERIC(48, 18),
     interval_pct_return NUMERIC(48, 18),
+    benchmark_btc_value NUMERIC(48, 18),
+    benchmark_btc_interval_pct_return NUMERIC(48, 18),
+    benchmark_eth_value NUMERIC(48, 18),
+    benchmark_eth_interval_pct_return NUMERIC(48, 18),
     PRIMARY KEY (id)
 );
+
+CREATE VIEW api.portfolio_history AS (
+    SELECT
+        *
+    FROM
+        portfolio_history
+);
+
+GRANT
+SELECT
+    on api.portfolio_history to api_anon;
