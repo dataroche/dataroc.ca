a373b6a10f3affe9466671cd6b18247f872f2669
Will S1200 <wil19558@gmail.com>
2023-10-10 10:57:25 -0400

    Fix api url trailing slash

--- lib/api/api.tsx
+++ lib/api/api.tsx
@@ -8,13 +8,16 @@ export type ApiProps = {
 }
 
 const SERVER_SIDE_CACHE_SECONDS = 10
+const _API_URL = process.env.REACT_APP_API_URL || ''
+
+const API_URL = _API_URL.endsWith('/') ? _API_URL.slice(0, -1) : _API_URL
 
 export function apiFetch<Type>({
   url,
   asSingleJsonObject,
   serverSide,
 }: ApiProps) {
-  const apiUrl = serverSide ? process.env.REACT_APP_API_URL + url : '/api' + url
+  const apiUrl = serverSide ? API_URL + url : '/api' + url
   const headers = {}
 
   if (asSingleJsonObject) {
