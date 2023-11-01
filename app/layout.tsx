189f6fe315ce121f4b6c84d45a3e26e545809238
Will S1200 <wil19558@gmail.com>
2023-10-31 21:28:13 -0400

    GA4, round 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,15 +64,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${textFont.className}`}>
-      <Head>
-        <GoogleAnalytics />
-      </Head>
+      <GoogleAnalytics />
       <body className="prose-invert bg-blue antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-16 md:mt-20 lg:mt-32 lg:mx-auto">
         <Topbar />
         <Sidebar />
         <main className="dark flex-auto min-w-0 mt-6 md:mt-0 flex flex-col">
           {children}
-          <Analytics />
         </main>
       </body>
     </html>
